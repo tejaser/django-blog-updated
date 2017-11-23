6a282b247375efc909a5e984676ca1f4b33d7ab5
Tejas Pancholi <tejas.er@outlook.com>
2017-11-23 19:26:21 +0530

    Add:template for list and detail page.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reserve
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
