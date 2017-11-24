3fc3dbbd37da074ed68459f0f2ed3fbaf9ec57de
Tejas Pancholi <tejas.er@outlook.com>
2017-11-24 12:23:02 +0530

    updated to class based view and templates.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
-def post_list(request):
+def post_list(request, category=None):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3) # 3 post in each page
     page = request.GET.get('page')
@@ -17,6 +18,14 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'page': page,
                                                    'posts': posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
