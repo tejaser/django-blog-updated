d90051135bf464076e2cef847d03041d14edad40
Tejas Pancholi <tejas.er@outlook.com>
2017-11-27 21:39:07 +0530

    Add:functionality for RSS feed.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from .feeds import LatestPostsFeed
+
 
 urlpatterns = [
     #post_views
@@ -10,4 +12,5 @@ urlpatterns = [
         views.post_detail,
         name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
